fb05f5682318d95ac65ee1224e2838855be34bbe
Liz00000000000 <Elizabeth.Michael.Karen@gmail.com>
2020-07-31 14:08:13 -0400

    newIcon

--- src/Components/ShowPosters.js
+++ src/Components/ShowPosters.js
@@ -3,20 +3,20 @@ import React from 'react'
 const ShowPosters = () => {
     return (
         <div className='ui two column centered grid'>
-            <div className='column'>
             <div>
+            <div className='column'>
                 <img src='http://2.bp.blogspot.com/-Y6wPt1AXtRM/UCAMFLFnQhI/AAAAAAAAAFE/NE-CTLj5vYs/s280/Valour.jpg' alt='octopus' />
             </div>
             <br></br>
-            <div>
+            <div className='column'>
                 <img src='http://4.bp.blogspot.com/-1GTnKVXbWH4/TpKd68_0KBI/AAAAAAAAABo/8otJs_DBMhY/s280/73174_445971260676_682775676_5814023_6268756_n.jpg' alt='cameo gallery' />
             </div>
             <br></br>
-            <div>
+            <div className='column'>
                 <img src='http://4.bp.blogspot.com/-2oh-PdaXKtI/TpKd9Oo-ZGI/AAAAAAAAABs/O7YyTdM7pG4/s280/74321_445973780676_682775676_5814048_70719_n.jpg' alt='cameo gallery' />
             </div>
             <br></br>
-            <div>
+            <div className='column'>
                 <img src='http://1.bp.blogspot.com/-VwBxavrW8_c/TpKd-5ZgmOI/AAAAAAAAABw/GcADX_ks13c/s280/168386_478900760676_682775676_6334047_3481899_n.jpg' alt='pary expo' />
             </div>
             </div>
