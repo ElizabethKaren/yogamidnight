9387aeced9c65189dcb8a4d19c09f3773f86a43e
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-08-30 14:00:28 -0400

    yogaPicSmallerAndCentered

--- src/Components/Yoga.js
+++ src/Components/Yoga.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import ContactMe from './ContactMe'
+import '../Styles/Yoga.css'
 
 const Yoga = () => {
  return (
    <div>
     <h1 className='ui purple header'><ContactMe/></h1>
-    <img className='ui fluid image' src='https://www.worldtopupdates.com/wp-content/uploads/2017/08/image-result-12-768x1024.jpeg' alt='yoga' />
+    <div id='imageCenter'>
+      <img className='ui large image' src='https://www.worldtopupdates.com/wp-content/uploads/2017/08/image-result-12-768x1024.jpeg' alt='yoga' />
+    </div>
    </div>
  )
 }
