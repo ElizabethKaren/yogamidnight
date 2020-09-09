c7abe4e4a16caef15cc8162d0fb26bcf74abc8ec
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-09-09 15:21:09 -0400

    taxiGifs

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import Sketches from './Components/Sketches'
 export class App extends Component {
 
   state = {
-    images: ['https://outline-prod.imgix.net/20190404-I43Z80hxDXOizldkH1s2?auto=format&q=60&w=2000&s=a5e8412c31d99b0610c69527c3a4432c', 'https://outline-prod.imgix.net/20190404-0CUA9tWXlYGHKujPu0EH?auto=format&q=60&w=2000&s=b1572eb32fc1eee0ca6eaa20a51ae8d1','https://outline-prod.imgix.net/20190404-t0UIhb2BBk2yx33TPI89?auto=format&q=60&w=2000&s=51d3b47805607e5902b76ba9b6e420ee','https://i1.wp.com/auralcrave.com/wp-content/uploads/2017/02/Lost-Highway.jpg?resize=810%2C515&ssl=1']
+    images: ['https://media2.giphy.com/media/6NAG3O1VDyUj6/giphy.gif','https://www.reactiongifs.us/wp-content/uploads/2014/03/you_talkin_to_me_robert_deniro.gif','https://media0.giphy.com/media/5eAnvHyewLV1m/giphy.gif','https://outline-prod.imgix.net/20190404-I43Z80hxDXOizldkH1s2?auto=format&q=60&w=2000&s=a5e8412c31d99b0610c69527c3a4432c', 'https://outline-prod.imgix.net/20190404-0CUA9tWXlYGHKujPu0EH?auto=format&q=60&w=2000&s=b1572eb32fc1eee0ca6eaa20a51ae8d1','https://outline-prod.imgix.net/20190404-t0UIhb2BBk2yx33TPI89?auto=format&q=60&w=2000&s=51d3b47805607e5902b76ba9b6e420ee','https://i1.wp.com/auralcrave.com/wp-content/uploads/2017/02/Lost-Highway.jpg?resize=810%2C515&ssl=1']
   }
   
   render(){
