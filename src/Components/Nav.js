c299b9978d707fac5e073387411f3d08886dfd88
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-12-10 13:33:58 -0500

    wordVChange

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -8,7 +8,7 @@ const Nav = () => {
      <h4>
          <NavLink to='/' className='ui purple header'>Home </NavLink>&nbsp;&nbsp;&nbsp;
          <NavLink to='/posters' className='ui purple header'>PosterArt </NavLink>&nbsp;&nbsp;&nbsp;
-         <NavLink to='/drawings' className='ui purple header'>Drawing </NavLink>&nbsp;&nbsp;&nbsp;
+         <NavLink to='/drawings' className='ui purple header'>Drawings </NavLink>&nbsp;&nbsp;&nbsp;
          <NavLink to='/art' className='ui purple header'>Multi-Media Art </NavLink>&nbsp;&nbsp;&nbsp;
          <NavLink to='/kungfu' className='ui purple header'>KungFu </NavLink>&nbsp;&nbsp;&nbsp;
          <NavLink to='/tarot' className='ui purple header'>Tarot </NavLink>&nbsp;&nbsp;&nbsp;
